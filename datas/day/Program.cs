09e7351cd7fd06fdba87312f92b09dd4c1d7ab1b
Alline <alline_franciely@hotmail.com>
2022-11-16 08:42:45 -0300

    manipulação de datas - day

---
+++ datas/day/Program.cs
@@ -0,0 +1,9 @@
+﻿public class DataUtil
+{
+  public static void Main(string[] rgs)
+  {
+    var date = new DateTime(2022, 10, 2, 8, 35, 0);
+    var dayOnly = date.Day;
+    Console.WriteLine(dayOnly.ToString());
+  }
+}
