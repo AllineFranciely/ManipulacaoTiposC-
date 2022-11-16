7f8f0e341faec4082a04b8ae8f65ad0e5518c505
Alline <alline_franciely@hotmail.com>
2022-11-16 08:46:00 -0300

    manipulação de datas - month

---
+++ datas/month/Program.cs
@@ -0,0 +1,9 @@
+﻿    public class DataUtil
+    {
+        public static void Main(string[] args)
+        {
+            var date = new DateTime(2022, 10, 2, 8, 35, 0);
+            var monthOnly = date.Month;
+            Console.WriteLine(monthOnly.ToString());
+        }
+    }
