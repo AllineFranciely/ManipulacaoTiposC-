d3cf808b37f263b974b238115f794a11fab4a21b
Alline <alline_franciely@hotmail.com>
2022-11-16 09:15:52 -0300

    manipulação de datas - year

---
+++ datas/year/Program.cs
@@ -0,0 +1,9 @@
+﻿    public class DataUtil
+    {
+        public static void Main(string[] args)
+        {
+            var date = new DateTime(2022, 10, 2, 8, 35, 0);
+            var yearOnly = date.Year;
+            Console.WriteLine(yearOnly.ToString());
+        }
+    }
