4a9826453ee6458c9d8b17b3cb46fb1cb9cabb02
Alline <alline_franciely@hotmail.com>
2022-11-16 08:35:57 -0300

    manipulação de datas - Now

---
+++ datas/now/Program.cs
@@ -0,0 +1,13 @@
+﻿public class DataUtil
+{
+  public static void Main(string[] args)
+  {
+    var dataType = DateTime.Now;
+    Console.WriteLine(dataType.GetType());
+  }
+
+  public string getTimeNow()
+  {
+    return "O momento de tempo atual é " +DateTime.Now;
+  }
+}
