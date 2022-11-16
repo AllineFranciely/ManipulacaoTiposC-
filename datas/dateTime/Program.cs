005caffc0bdfdc998b7dbd36da6e9020f1b640fd
Alline <alline_franciely@hotmail.com>
2022-11-16 08:32:18 -0300

    manipulação de datas - dateTime

---
+++ datas/dateTime/Program.cs
@@ -0,0 +1,8 @@
+﻿public class DataUtil
+{
+    public static void Main(string[] args)
+    {
+        var date = new DateTime(2022, 10, 2, 8, 35, 0);
+        Console.WriteLine(date.ToString());
+    }
+}
